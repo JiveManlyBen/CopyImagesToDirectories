454937bf769a96b33cb9523f9769dbe62eb28cea
Ben Levy <contact@benlevy.com>
2013-11-16 15:16:25 -0600

    Moved main functionality to a function

--- CopyImagesToDirectories.py
+++ CopyImagesToDirectories.py
@@ -35,53 +35,55 @@ def get_exif_time(file_path):
     except IOError, e:
         return None
 
+def copy_image_files(in_path, out_path):
+	message = ""
+	copied_image_count = 0
+	failed_image_count = 0
+	try:
+		for f in os.listdir(in_path):
+			if os.path.isfile(os.path.join(in_path, f)):
+				if f.lower().endswith(".jpg") or f.lower().endswith(".cr2") or f.lower().endswith(".mov"):
+					image_time = get_exif_time(in_path + f)
+					if image_time is None:
+						image_time = get_file_system_time(in_path + f)
+					y_path = time.strftime("%Y", image_time)
+					m_path = time.strftime("%Y" + os.path.sep +"%Y_%m", image_time)
+					d_path = time.strftime("%Y" + os.path.sep +"%Y_%m" + os.path.sep + "%Y_%m_%d", image_time)
+					f_path = os.path.join(os.path.join(out_path, d_path), f)
+					if not os.path.exists(os.path.join(out_path, d_path)):
+						if os.path.exists(os.path.join(out_path, m_path)):
+							os.makedirs(os.path.join(out_path, d_path))
+						else:
+							if os.path.exists(os.path.join(out_path, y_path)):
+								os.makedirs(os.path.join(out_path, m_path))
+								os.makedirs(os.path.join(out_path, d_path))
+							else:
+								os.makedirs(os.path.join(out_path, y_path))
+								os.makedirs(os.path.join(out_path, m_path))
+								os.makedirs(os.path.join(out_path, d_path))
+
+					if (not os.path.isfile(f_path)):
+						print "copying " + os.path.join(in_path, f)
+						shutil.copy2(os.path.join(in_path, f), f_path)
+						copied_image_count += 1
+					elif not filecmp.cmp(os.path.join(in_path, f), f_path):
+						print "warning: file mismatch for \"" + f_path + "\""
+						failed_image_count += 1
+	except (IOError, OSError), e:
+		message = str(e)
+	finally:
+		if len(message) > 0:
+			message = "\n" + message
+		if failed_image_count > 0:
+			message = " (with " + str(failed_image_count) + " errors)" + message
+		message = "Copied " + str(copied_image_count) + " new images from \"" + in_path + "\" to \"" + out_path + "\"" + message
+		return message
+
 try:
 	in_path = add_trailing_separator(sys.argv[1])
 	out_path = add_trailing_separator(sys.argv[2])
+	print copy_image_files(in_path, out_path)
 except IndexError, e:
 	print "required input and output directories not supplied: " + str(e)
 	print "\n" + get_example_text()
 	exit()
-
-message = ""
-copied_image_count = 0
-failed_image_count = 0
-try:
-	for f in os.listdir(in_path):
-		if os.path.isfile(os.path.join(in_path, f)):
-			if f.lower().endswith(".jpg") or f.lower().endswith(".cr2") or f.lower().endswith(".mov"):
-				image_time = get_exif_time(in_path + f)
-				if image_time is None:
-					image_time = get_file_system_time(in_path + f)
-				y_path = time.strftime("%Y", image_time)
-				m_path = time.strftime("%Y" + os.path.sep +"%Y_%m", image_time)
-				d_path = time.strftime("%Y" + os.path.sep +"%Y_%m" + os.path.sep + "%Y_%m_%d", image_time)
-				f_path = os.path.join(os.path.join(out_path, d_path), f)
-				if not os.path.exists(os.path.join(out_path, d_path)):
-					if os.path.exists(os.path.join(out_path, m_path)):
-						os.makedirs(os.path.join(out_path, d_path))
-					else:
-						if os.path.exists(os.path.join(out_path, y_path)):
-							os.makedirs(os.path.join(out_path, m_path))
-							os.makedirs(os.path.join(out_path, d_path))
-						else:
-							os.makedirs(os.path.join(out_path, y_path))
-							os.makedirs(os.path.join(out_path, m_path))
-							os.makedirs(os.path.join(out_path, d_path))
-
-				if (not os.path.isfile(f_path)):
-					print "copying " + os.path.join(in_path, f)
-					shutil.copy2(os.path.join(in_path, f), f_path)
-					copied_image_count += 1
-				elif not filecmp.cmp(os.path.join(in_path, f), f_path):
-					print "warning: file mismatch for \"" + f_path + "\""
-					failed_image_count += 1
-except (IOError, OSError), e:
-	message = str(e)
-finally:
-	if len(message) > 0:
-		message = "\n" + message
-	if failed_image_count > 0:
-		message = " (with " + str(failed_image_count) + " errors)" + message
-	message = "Copied " + str(copied_image_count) + " new images from \"" + in_path + "\" to \"" + out_path + "\"" + message
-	print message
